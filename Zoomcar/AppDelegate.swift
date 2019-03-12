1b5665d3f283446b236ff73dd3eddbc84169e8ff
Prithvi Venu <prithviv30@gmail.com>
2019-03-12 15:03:17 +0530

    Initial Commit

---
+++ Zoomcar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Zoomcar
+//
+//  Created by prithvi-pt2335 on 12/03/19.
+//  Copyright © 2019 prithvi-pt2335. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Zoomcar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Zoomcar
+//
+//  Created by prithvi-pt2335 on 12/03/19.
+//  Copyright © 2019 prithvi-pt2335. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
